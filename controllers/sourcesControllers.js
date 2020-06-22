4df9e88ffe14caadc0d8f572a737f3a006dccc84
moazGalbat <moaz.galbat@gmail.com>
2020-06-22 23:18:44 +0200

    remove unused files

--- controllers/sourcesControllers.js
+++ controllers/sourcesControllers.js
@@ -6,15 +6,6 @@ const apiKey = process.env.NEWS_API_KEY
 const getAllSources = async (req, res, next) => {
     try {
         const response = await axios.get(`https://newsapi.org/v2/sources?apiKey=${apiKey}`);
-        
-        // SourceModel.collection.insertMany(response.data.sources, onInsert);
-        // function onInsert(err, docs) {
-        //     if (err) {
-        //         console.log(err);
-        //     } else {
-        //         console.info('%d potatoes were successfully stored.', docs.length);
-        //     }
-        // }
         res.status(200).json(response.data.sources)
     } catch (error) {
         next(error)   
